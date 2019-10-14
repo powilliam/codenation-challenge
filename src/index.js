c1348f3f8f1ef1ed536f0867e0e52f035c8bdfe5
powilliam <powilliam19@gmail.com>
2019-10-14 10:43:10 -0400

    add -> process.stdout.write() in some strings, function to convert json to string

--- src/index.js
+++ src/index.js
@@ -12,86 +12,96 @@ const api = axios.create({
     baseURL: process.env.URL
 });
 
-async function Start() {
-    console.log('Starting Challenge!');
+const answerPath = path.resolve(__dirname, '..', 'private', 'answer.json');
 
-    const answerPath = path.resolve(__dirname, '..', 'private', 'answer.json');
+async function Start() {
+    process.stdout.write('Starting Challenge!\n');
     const responseJSON = await getChallenge();
 
     const { numero_casas, token, cifrado, decifrado, resumo_criptografico } = responseJSON;
-    const convertedResponse = JSON.stringify(responseJSON)
 
+    const convertedResponse = convertToString(responseJSON);
     await writeChallenge(answerPath, convertedResponse);
 
     const decrypted = decryptChallenge(cifrado, 6);
     const hash = generateSHA1(decrypted);
 
-    const content = {
+    const convertedRequest = convertToString({
         numero_casas,
         token,
         cifrado,
         decifrado: decrypted,
         resumo_criptografico: hash,
-    }
+    });
 
-    await updateChallenge(answerPath, JSON.stringify(content));
+    await updateChallenge(answerPath, convertedRequest);
     await sendChallenge(answerPath);
+}
+
+const convertToString = (string) => {
+    return JSON.stringify(string);
 }
 
 const getChallenge = async () => {
     try {
         const response = await api.get(`/generate-data?token=${process.env.TOKEN}`)
-        const { data } = response;
+        const { status, data } = response;
 
-        console.log('API GET STATUS CODE: 200 \n');
+        console.log(`Request STATUS: ${status}`);
+
+        if (status == 200) {
+            process.stdout.write('Success!\n');
+        }
+
         return data;
     } catch (error) {
-        console.log(error)
+        console.err(error);
     }
 }
 
 const writeChallenge = async (path, content) => {
-    console.log('Creating answer.json...')
+    process.stdout.write('Creating answer.json...\n')
     try {
         fs.writeFileSync(path, content);
-        console.log('answer.json created! \n');
+        process.stdout.write('answer.json created!\n\n');
     } catch (error) {
-        console.log('Error when trying to create answer.json')
+        console.err('Error when trying to create answer.json \n')
     }
 }
 
 const decryptChallenge = (content, houses) => {
-    let buildString = Caesar.Decipher(houses).crypt(content);
+    let deciphred = Caesar.Decipher(houses).crypt(content);
     
-    console.log('Trying to discover what is that...')
-    console.log(`The message is: ${buildString} \n`);
+    process.stdout.write('Trying to discover what is that...\n')
+    console.log(`The message is: "${deciphred}" \n`);
 
-    return buildString;
+    return deciphred;
 }
 
 const generateSHA1 = (content) => {
-    console.log('Generating a SHA1 HASH...')
+    process.stdout.write('Generating a SHA1 HASH...\n')
     try {
         const hash = sha1(content);
         console.log(`The hash is: ${hash}\n`);
         return hash
     } catch (error) {
-        console.log('Cannot create a hash');
+        console.err('Cannot create a hash\n');
     }
 }
 
 const updateChallenge = async (path, content) => {
-    console.log('Updating answer.json...');
+    process.stdout.write('Updating answer.json...\n');
     try {
         fs.writeFileSync(path, content);
         console.log('Update successfull \n');
     } catch (error) {
-        console.log('Error when trying to overwrite answer.json \n\n')
+        console.err('Error when trying to overwrite answer.json \n')
     }
 }
 
 const sendChallenge = async (path) => {
-    console.log('Uploading challenge...')
+    process.stdout.write('Uploading challenge...\n')
+
     const data = new FormData();
     data.append('answer', fs.createReadStream(path))
 
@@ -103,8 +113,8 @@ const sendChallenge = async (path) => {
         );
         const { score } = response.data;
 
-        console.log(`Your score is: ${score}`);
-        console.log('Done!');
+        console.log(`Your score is: ${score}\n`);
+        process.stout.write('Done!');
     } catch (error) {
         console.log(error)
     }
